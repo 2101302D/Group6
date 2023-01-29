fdd21c6f48bff3a9002069b14d0ae68c0e39418e
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-29 14:02:21 +0800

    Started with unit testing by adding in JUnit 5, Version 2.17

---
+++ src/main/java/com/dvops/maven/eclipse/KeyboardTest.java
@@ -0,0 +1,70 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class KeyboardTest {
+	private KeyboardTest kt;
+	private keyboard k1;
+	private keyboard k2;
+	private final int KEYBOARD_COLLECTION_SIZE = 2;
+	
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSwitches() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBacklight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPassthrough() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetKeycaps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRating() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetImage() {
+		fail("Not yet implemented");
+	}
+	
+
+}
