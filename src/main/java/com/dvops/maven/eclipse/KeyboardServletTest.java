8eea588d52d5084964658622525526baec6ccf43
JIAWEI68 <ang.jiawei56@gmail.com>
2023-01-29 22:21:33 +0800

    Fixed the import issue for mockito and added mockito

---
+++ src/main/java/com/dvops/maven/eclipse/KeyboardServletTest.java
@@ -0,0 +1,49 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+@RunWith(MockitoJUnitRunner.class)
+class KeyboardServletTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse1() {
+		fail("Not yet implemented");
+	}
+
+}
