d31cac3518e28c952d9c25f83034655fc368e4db
2101302D <2101302D@student.tp.edu.sg>
2023-01-31 02:56:43 +0800

    Finish setting up Unit Testing for Mouse Collection

---
+++ src/test/java/com/dvops/maven/eclipse/MouseCollectionTest.java
@@ -0,0 +1,19 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MouseCollectionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetMouses() {
+		fail("Not yet implemented");
+	}
+
+}
