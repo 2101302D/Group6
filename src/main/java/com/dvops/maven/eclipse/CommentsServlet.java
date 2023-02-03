5c0824696c28d9c9c59a08696d875b18c4da4790
2101302D <2101302D@student.tp.edu.sg>
2023-02-04 02:38:07 +0800

    fix: new location of Comment, Register, and User Servlet

---
+++ src/main/java/com/dvops/maven/eclipse/CommentsServlet.java
@@ -0,0 +1,71 @@
+package com.dvops.maven.eclipse;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CommentsServlet
+ */
+@WebServlet("/CommentsServlet")
+public class CommentsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CommentsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String n = request.getParameter("userName");
+		String e = request.getParameter("email");
+		String c = request.getParameter("feedback");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(
+			"jdbc:mysql://localhost:3306/userdetails", "root", "password");
+			
+		PreparedStatement ps = con.prepareStatement("insert into name values(?,?,?)");
+		ps.setString(1, n);
+		ps.setString(2, e);
+		ps.setString(3, c);
+		int i = ps.executeUpdate();
+		if (i > 0){
+			PrintWriter writer = response.getWriter();
+			writer.println("<h1>" + "You have submitted the comment!" + "</h1>");
+			writer.close();
+		}
+		}
+		catch (Exception exception) {
+			System.out.println(exception);
+			}
+			doGet(request, response);
+	}
+
+}
+
