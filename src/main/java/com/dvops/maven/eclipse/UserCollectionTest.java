1b8fda50df7335f0146088d89eb5a4e9619ef195
austi <austinseahhr>
2023-02-05 23:44:08 +0800

    add :Fix user collection unit testing

--- src/main/java/com/dvops/maven/eclipse/UserCollectionTest.java
+++ src/main/java/com/dvops/maven/eclipse/UserCollectionTest.java
@@ -3,7 +3,7 @@ package com.dvops.maven.eclipse;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserCollectionTest {
@@ -14,6 +14,7 @@ class UserCollectionTest {
 	private User u3;
 	private final int USER_COLLECTION_SIZE = 3;
 	
+	@BeforeEach
 	void setUp() throws Exception {
 		uc = new UserCollection();
 		u1 = new User("yes","yes","yes@gmail.com");
@@ -24,21 +25,26 @@ class UserCollectionTest {
 		uc.addUser(u2);
 		uc.addUser(u3);
 	}
+	@Test
 	void testAddUser(){
 		uc.addUser(u1);
         List<User> testUc = uc.getUser();
 	}
+	@Test
 	void testGetUser() {
 		List<User> testUc = uc.getUser();
 	}
+	@Test
 	void testGetByName() {
 		User testUc = uc.getByName("yes");
 	}
+	@Test
 	void testUpdateUser() {
 		uc.addUser(u3);
 		uc.updateUser("Change", "Change", "change@email.com");
 		List<User> testuc = uc.getUser();
 	}
+	@Test
 	void testDeleteUser() {
 		uc.addUser(u3);
 		List<User> beforeUc = uc.getUser();
